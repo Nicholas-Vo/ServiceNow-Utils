776a948f37d0660c43cc3d07f798a79ff8d14446
Nicholas-Vo <nvoss@me.com>
2023-07-06 09:04:08 -0500

    Initial commit

---
+++ HTMLUtils.js
@@ -0,0 +1,15 @@
+function decodeHTML(str) {
+    var a = str.replace(/<\/?[^>]+(>|$)/g, ""); //Remove tags
+    var b = a.replace(/&amp;/g, '&'); //Retain any ampersands that are just ampersands
+    return b.replace(/&#(\d+);/g, function(match, dec) {
+        return String.fromCharCode(dec); //Returns the special character from the decimal code representation and returns the entire decoded string.
+    });
+}
+
+function removeHTML(text) {
+    var lineBreakRegex = /<br\s\/>/ig;
+    var tagRegex = /<\/?[a-z][\s\S]*?(?:\/>|>)/ig;
+
+    var temp = text.replace(lineBreakRegex, '\r\n');
+    return temp.replace(tagRegex, "");
+}
