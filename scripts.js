f080983673d2a9e623592ec8ed499945313ad851
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2023-07-13 08:53:39 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+/* Purge records from a table */
+var theTable = "tablename";
+var limit = 1000; // Set a limit - possible performance impact?
+  
+var gr = new GlideRecord(theTable);
+gr.addNullQuery('name'); // Will query all records with null "name" field.
+gr.setLimit(limit);
+gr.query();
+gr.deleteMultiple();
+gs.info("Deleted " + gr.getRowCount() + " empty/null records from" + theTable + " table.");
