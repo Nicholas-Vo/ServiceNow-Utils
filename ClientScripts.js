9f792b4bb3ce0e788d2d8cbdc0b291cdf29f0b47
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2023-07-17 12:49:58 -0500

    Create ClientScripts.js

---
+++ ClientScripts.js
@@ -0,0 +1,21 @@
+/* Client script to change a field based off of the value of another field.
+In this instance, when the agent updates the opened_for field, we want the
+location field to automatically populate.
+
+UI Type: All
+Type: OnChange
+Field name: Opened for
+Global: true
+
+ */
+function onChange(control, oldValue, newValue, isLoading) {
+    if (isLoading || newValue === '') {
+        return;
+    }
+
+    g_form.getReference('opened_for', setUserLocationCallback);
+}
+
+function setUserLocationCallback(user) {
+    g_form.setValue('location', user.location);
+}
