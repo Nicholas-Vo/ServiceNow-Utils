71c3b8a0f415352df55f0dbfc87b3dad8ece060c
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2023-07-30 20:04:00 -0500

    Update self-executing-functions.js

--- self-executing-functions.js
+++ self-executing-functions.js
@@ -3,8 +3,8 @@
 A self-executing function is both declared and invoked within the same script field.
 Whenever writing a script that only needs to run in a single context, use this type of function.
 For functions that must run in multiple contexts, consider reusable functions instead.
-By enclosing your script in a self-executing function you can ensure that the script does not impact other areas of the product, such as by overwriting global variables.
-
+By enclosing your script in a self-executing function you can ensure that the script does not 
+impact other areas of the product, such as by overwriting global variables.
 */
 
 (function functionName() {
