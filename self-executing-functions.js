ba264c834a2534ab9a5330dedd396051a9941408
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2023-08-08 13:27:38 -0500

    Update self-executing-functions.js

--- self-executing-functions.js
+++ self-executing-functions.js
@@ -2,9 +2,14 @@
 
 A self-executing function is both declared and invoked within the same script field.
 Whenever writing a script that only needs to run in a single context, use this type of function.
+
 For functions that must run in multiple contexts, consider reusable functions instead.
 By enclosing your script in a self-executing function you can ensure that the script does not 
 impact other areas of the product, such as by overwriting global variables.
+
+Selct-executing functions, or IIFEs (Immediately Invoked Function Expressions) are best practice in
+ServiceNow as individual business rules, UI Actions, etc. are actually within the same scope as other
+business rules or UI Actions. This can result in inadvertant behavior should functions have the same name!
 */
 
 (function functionName() {
