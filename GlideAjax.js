ba6f6811d5b814530d346a44bc299ddfc7403f7a
Nick <60233722+Nicholas-Vo@users.noreply.github.com>
2024-05-02 09:38:03 -0500

    Update GlideAjax.js

--- GlideAjax.js
+++ GlideAjax.js
@@ -5,9 +5,9 @@ function onLoad() {
     var a_glide_ajax = new GlideAjax('NickAJAXUtil');
     a_glide_ajax.addParam('sysparm_name', 'getUserLocation');
     a_glide_ajax.addParam('sysparm_user', g_form.getValue('requested_for'));
-	
-    a_glide_ajax.getXMLAnswer(function(response) {
-		g_form.setValue('location', response);
+
+    a_glide_ajax.getXMLAnswer(function (response) {
+        g_form.setValue('location', response);
     });
 }
 
@@ -17,8 +17,8 @@ AbstractAjaxProcessor utility Script Include
 var NickAJAXUtil = Class.create();
 NickAJAXUtil.prototype = Object.extendsObject(AbstractAjaxProcessor, {
 
-    getUserLocation: function() {
-		var user = this.getParameter('sysparm_user');
+    getUserLocation: function () {
+        var user = this.getParameter('sysparm_user');
 
         var gr_user = new GlideRecord('sys_user');
         if (gr_user.get(user)) {
